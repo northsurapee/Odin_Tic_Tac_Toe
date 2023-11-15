1171157051ae8e0d67081386d52adb79deee618b
Surapee <suwan.srp@gmail.com>
2023-11-15 15:39:01 +0700

    Done this game with minimal features

--- script.js
+++ script.js
@@ -1,103 +1,152 @@
-function Gameboard() {
-    const board = [[Cell(),Cell(),Cell()],
-                   [Cell(),Cell(),Cell()],
-                   [Cell(),Cell(),Cell()]];
-    const getBoard = () => board;
-    const selectCell = (row, column, playerMark) => {
-        if (board[row][column].getMark() === '-') {
-            board[row][column].addMark(playerMark)
-        }
-    }
-    const printBoard = () => {
-        const boardWithCellMarks = board.map((row) => row.map((cell) => cell.getMark()))
-        console.log(boardWithCellMarks)
-    }
-    const isGameEnd = () => {
-        // Check row
-        for (let i = 0; i < 3; i++) {
-            if (board[i][0].getMark() != '-' && board[i][0].getMark() === board[i][1].getMark() && board[i][1].getMark() === board[i][2].getMark()) {
-                return true
-            }
-        }
-        // Check column
-        for (let j = 0; j < 3; j++) {
-            if (board[0][j].getMark() != '-' && board[0][j].getMark() === board[1][j].getMark() && board[1][j].getMark() === board[2][j].getMark()) {
-                return true
-            }
-        }
-        // Check diagonal
-        if (board[1][1].getMark() != '-' && board[0][0].getMark() === board[1][1].getMark() && board[1][1].getMark() === board[2][2].getMark()) {
-            return true
-        } else if (board[1][1].getMark() != '-' && board[2][0].getMark() === board[1][1].getMark() && board[1][1].getMark() === board[0][2].getMark()) {
-            return true
-        }
-        return false 
-    }
-    
-    return {getBoard, selectCell, printBoard, isGameEnd}
-}
-
+// Cell FACTORY FUNCTION (many -> factory)
 function Cell() {
-    let mark = '-'
-    const addMark = (playerMark) => {
-        mark = playerMark
-    }
+    let mark = ''
+    const addMark = (playerMark) => {mark = playerMark}
     const getMark = () => mark;
     return {addMark, getMark}
 }
 
-function Player(
-    playerOneName = "Player One",
-    playerTwoName = "Player Two"
-  ) {
-    const players = [
-        {
-            name: playerOneName,
-            mark: 'X'
-        },
-        {
-            name: playerTwoName,
-            mark: 'O'
+
+
+// GAME BOARD MODULE PATTERN (wrap the factory function inside an IIFE)
+const gameBoard = (() => {
+    let board = [[Cell(),Cell(),Cell()], // no need 'new' keyword because this is factory function!
+                 [Cell(),Cell(),Cell()],
+                 [Cell(),Cell(),Cell()]];
+
+    const reset = () => {
+        board = [[Cell(),Cell(),Cell()],
+                 [Cell(),Cell(),Cell()],
+                 [Cell(),Cell(),Cell()]];
+    }
+
+    const getBoard = () => board;
+
+    const selectCell = (row, column, playerMark) => {board[row][column].addMark(playerMark)}
+
+    const isGameEnd = () => {
+        // Check row and column
+        for (let i = 0; i < 3; i++) {
+            if ((board[i][0].getMark() !== '' && board[i][0].getMark() === board[i][1].getMark() && board[i][1].getMark() === board[i][2].getMark()) ||
+                (board[0][i].getMark() !== '' && board[0][i].getMark() === board[1][i].getMark() && board[1][i].getMark() === board[2][i].getMark())) {
+                return true;
+            }
         }
-    ]
+        // Check diagonal
+        if ((board[1][1].getMark() != '' && board[0][0].getMark() === board[1][1].getMark() && board[1][1].getMark() === board[2][2].getMark()) ||
+            (board[1][1].getMark() != '' && board[2][0].getMark() === board[1][1].getMark() && board[1][1].getMark() === board[0][2].getMark())) {
+            return true
+        } 
+        return false 
+    }
+
+    const isGameTie = () => {
+        for(let i = 0; i<3; i++) {
+            for(let j = 0; j<3; j++) {
+                if(board[i][j].getMark() === '') {return false}
+            }
+        }
+        return true
+    }
+
+    const getCellMark = (row, column) => {
+        return board[row][column].getMark()
+    }
+
+    const isCellAvailable = (row,column) => {
+        return board[row][column].getMark() === ''
+    }
+    
+    return {getBoard, selectCell, isGameEnd, getCellMark, isCellAvailable, reset, isGameTie}
+})();
+
+
+
+// PLAYER MODULE PATTERN
+const player = ((playerOneName = "Player One", playerTwoName = "Player Two") => {
+    let players = [{name: playerOneName,mark: 'X'}, {name: playerTwoName,mark: 'O'}]
     let activePlayer = players[0]
+    
+    const reset = () => {
+        players = [{name: playerOneName,mark: 'X'}, {name: playerTwoName,mark: 'O'}]
+        activePlayer = players[0]
+    }
+    
     const switchPlayerTurn = () => {
         activePlayer = activePlayer === players[0] ? players[1] : players[0]
     }
-    // Getter
+    
     const getActivePlayer = () => activePlayer
-    return {switchPlayerTurn, getActivePlayer}
-  }
+    return {switchPlayerTurn, getActivePlayer, reset}
+  })();
 
 
-function GameController() {
-    const board = Gameboard()
-    const player = Player()
-    let isGameEnd = false
 
-    const printNewRound = () => {
-        board.printBoard()
-        console.log(`${player.getActivePlayer().name}'s turn.`)
-    }
+// GAME CONTROLLER MODULE PATTERN
+const gameController = (() => {
+    let subtitles = ''
 
     const playRound = (row, column) => {
+        // Do nothing if game end or cell is unavailable
+        if (gameBoard.isGameEnd() || gameBoard.isGameTie() || !gameBoard.isCellAvailable(row, column)) {return}
         // Select cell
-        board.selectCell(row, column, player.getActivePlayer().mark)
+        gameBoard.selectCell(row, column, player.getActivePlayer().mark)
         console.log(`${player.getActivePlayer().name} mark on (${row},${column})`)
-
-        if(!board.isGameEnd()) {
-            player.switchPlayerTurn()
-            printNewRound()
+        subtitles = `${player.getActivePlayer().name} mark on (${row},${column})`
+        // Check game end / tie / continue
+        if(gameBoard.isGameEnd()) {
+            subtitles = `Game end, the winner is ${player.getActivePlayer().name}!`
+        } else if(gameBoard.isGameTie()) {
+            subtitles = `Game tie, there are no winner kubb!`
         } else {
-            console.log(`Game end! The winner is ${player.getActivePlayer().name}!`)
+            player.switchPlayerTurn()
+            subtitles = `${player.getActivePlayer().name}'s turn.`
         }
+        render(gameBoard, subtitles)
+    }
+
+    // Restart
+    const restartBtn = document.querySelector('.restart')
+    restartBtn.addEventListener('click', () => gameController.playAgain())
+    const playAgain = () => {
+        gameBoard.reset()
+        player.reset()
+        subtitles = `${player.getActivePlayer().name}'s turn.`
+        render(gameBoard, subtitles)
     }
 
     // Initial play game message
-    printNewRound()
+    subtitles = `${player.getActivePlayer().name}'s turn.`
+    render(gameBoard, subtitles)
 
-    return {playRound}
+    return {playRound, playAgain}
+})();
+
+
+
+// RENDER FUNCTION
+function render(board, subtitles) {
+    // clear board
+    const boardDiv = document.querySelector('.board')
+    boardDiv.innerHTML = ''
+    // render board
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            // create cell div
+            let cellDiv = document.createElement("div")
+            cellDiv.classList.add('cell')
+            cellDiv.textContent = board.getCellMark(i,j)
+            boardDiv.appendChild(cellDiv)
+            // add listener to cell div
+            cellDiv.addEventListener('click', () => {gameController.playRound(i,j)})
+        }
+    }
+    // Set subtitle
+    const subtitlesDiv = document.querySelector('.subtitles')
+    subtitlesDiv.textContent = subtitles
 }
 
-const game = GameController()
+/* 
+no main called here because GAME CONTROLLER is main and it IIFE
+*/
 
